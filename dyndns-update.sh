ef00b07f7dd4cf7fe63e05f085891a3e7280f456
Janik Rabe <mail@janik-rabe.de>
2019-09-29 20:29:47 +0200

    Get public ip address and hostname ip address

--- dyndns-update.sh
+++ dyndns-update.sh
@@ -17,3 +17,14 @@ v6_get_url="https://v6.ident.me/"
 dyndns_update_url="https://dyndns.inwx.com/nic/update?"
 
 test_dns_server="1.1.1.1"
+
+
+## Start Bash Script
+
+# Get public IP Address
+ipv4=$(curl -s "$v4_get_url")
+ipv6=$(curl -s "$v6_get_url")
+
+# Get current hostname IP address
+host_ipv4=$(dig @$test_dns_server +short -t a $hostname | head -n 1)
+host_ipv6=$(dig @$test_dns_server +short -t aaaa $hostname | head -n 1)
