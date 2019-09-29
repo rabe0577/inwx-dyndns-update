bce3078b4061d3dab76079f392e46d52fb3c2515
Janik Rabe <mail@janik-rabe.de>
2019-09-29 20:27:33 +0200

    Add optional script variables

--- dyndns-update.sh
+++ dyndns-update.sh
@@ -6,3 +6,14 @@ username='user'
 password='password'
 
 hostname='your-dyndns.domain.tld'
+
+# Optional Script setting
+
+enable_ipv6=true
+
+v4_get_url="https://v4.ident.me/"
+v6_get_url="https://v6.ident.me/"
+
+dyndns_update_url="https://dyndns.inwx.com/nic/update?"
+
+test_dns_server="1.1.1.1"
