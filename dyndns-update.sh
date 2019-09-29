f787ba5858c4aa2eac43dfa77ae95cef1071a063
Janik Rabe <mail@janik-rabe.de>
2019-09-29 20:30:37 +0200

    Update IP Address if not correct

--- dyndns-update.sh
+++ dyndns-update.sh
@@ -28,3 +28,14 @@ ipv6=$(curl -s "$v6_get_url")
 # Get current hostname IP address
 host_ipv4=$(dig @$test_dns_server +short -t a $hostname | head -n 1)
 host_ipv6=$(dig @$test_dns_server +short -t aaaa $hostname | head -n 1)
+
+# Update DynDNS IP if not correct
+if [ $enable_ipv6 = true -a -n "$ipv6" ]; then
+  if [ $host_ipv4 != $ipv4 -o $host_ipv6 != $ipv6 ]; then
+    curl --user $username:$password ${dyndns_update_url}myip=${ipv4}&myipv6=${ipv6}
+  fi
+else
+  if [ $host_ipv4 != $ipv4 ]; then
+    curl --user $username:$password ${dyndns_update_url}myip=${ipv4}
+  fi
+fi
