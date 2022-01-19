43b8fd7f85fa15116e2b93f2710382c5c30412fe
Dominic Brekau <35068626+dbrekau@users.noreply.github.com>
2022-01-19 01:07:54 +0100

    Put URL into double quotes (#3)
    Co-authored-by: Dominic <dominic@xdnx.org>

--- dyndns-update.sh
+++ dyndns-update.sh
@@ -32,10 +32,10 @@ host_ipv6=$(dig @$test_dns_server +short -t aaaa $hostname | head -n 1)
 # Update DynDNS IP if not correct
 if [ $enable_ipv6 = true -a -n "$ipv6" ]; then
   if [ "$host_ipv4" != "$ipv4" -o "$host_ipv6" != "$ipv6" ]; then
-    curl --user $username:$password ${dyndns_update_url}myip=${ipv4}&myipv6=${ipv6}
+    curl --user $username:$password "${dyndns_update_url}myip=${ipv4}&myipv6=${ipv6}"
   fi
 else
   if [ "$host_ipv4" != "$ipv4" ]; then
-    curl --user $username:$password ${dyndns_update_url}myip=${ipv4}
+    curl --user $username:$password "${dyndns_update_url}myip=${ipv4}"
   fi
 fi
